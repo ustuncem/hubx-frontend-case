df8f765bc61756029a9c322e07b920635ea8b3cf
ustuncem <ustuncem7@gmail.com>
2024-05-31 23:45:15 +0300

    chore: implement Inter font and public folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ export function App() {
   return (
     <main className="h-full">
       <section id="content-container">
-        <img />
         <article>
-          <p>Document Scanner</p>
-          <h1>Scan With Ease</h1>
-          <p>
-            Scan any document instantly with your mobile device by just a few
-            steps. Save as PDF,JPG,ZIP,TXT and Word format.
-          </p>
-          <a href="#">Learn More</a>
+          <div>
+            <p>Document Scanner</p>
+            <h1>Scan With Ease</h1>
+            <p>
+              Scan any document instantly with your mobile device by just a few
+              steps. Save as PDF,JPG,ZIP,TXT and Word format.
+            </p>
+            <a href="#">Learn More</a>
+          </div>
+          <img className="" alt="" src="/img/document-scanner.png" />
         </article>
       </section>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#0B122A',
+        action: '#0381FF',
+      },
+    },
   },
   plugins: [],
 };

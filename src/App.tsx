2e280651d2a89d4178dee15be98523ef5ed23649
ustuncem <ustuncem7@gmail.com>
2024-05-31 23:18:45 +0300

    chore: basic skeleton and layout base implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import styles from './App.module.css';
-
 export function App() {
   return (
-    <>
-      <header>
-        <h1 className="text-6xl">HubX Frontend Assignment</h1>
-      </header>
-
-      <main className={styles.main}></main>
-    </>
+    <main className="h-full">
+      <section id="content-container">
+        <img />
+        <article>
+          <p>Document Scanner</p>
+          <h1>Scan With Ease</h1>
+          <p>
+            Scan any document instantly with your mobile device by just a few
+            steps. Save as PDF,JPG,ZIP,TXT and Word format.
+          </p>
+          <a href="#">Learn More</a>
+        </article>
+      </section>
+    </main>
   );
 }

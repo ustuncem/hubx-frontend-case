5e34520b1853038c1e89e41f2e006a02546ad1da
ustuncem <ustuncem7@gmail.com>
2024-06-02 17:09:44 +0300

    chore: add commitlint and husky

---
+++ commitlint.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'init',
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'revert',
+        'perf',
+        'version-update',
+      ],
+    ],
+  },
+};

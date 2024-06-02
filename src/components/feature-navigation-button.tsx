4ecf7ddac62d41701f519418f1964bc26ac9aacd
ustuncem <ustuncem7@gmail.com>
2024-06-03 00:16:57 +0300

    feat: feature-navigation-button animation completed

--- src/components/feature-navigation-button.tsx
+++ src/components/feature-navigation-button.tsx
@@ -26,7 +26,7 @@ export default function FeatureNavigationButton({
     <button
       role="button"
       onClick={onClick}
-      className={`flex w-full min-w-[280px] flex-1 items-center justify-center gap-x-4 border-r border-neutral-300 px-6 py-4 transition-colors last:border-r-0 xl:min-w-[270px] ${isActive ? 'bg-action/5' : 'bg-white'}`}
+      className={`flex w-full min-w-[280px] flex-1 cursor-pointer items-center justify-center gap-x-4 border-r border-neutral-300 px-6 py-4 transition-colors last:border-r-0 xl:min-w-[270px] ${isActive ? 'animate-feature-svg bg-action/5' : 'bg-white'}`}
     >
       <FeatureNavigationButton.Svg svgPath={svgPath} />
       {buttonLabel}
